8799e5fdf65252c0fdf50a3c03c6e57b04d006d5
DaVinci192 <58537048+DaVinci192@users.noreply.github.com>
2021-10-06 23:58:30 -0400

    added CountOnes function

--- src/main.cpp
+++ src/main.cpp
@@ -6,11 +6,27 @@ Inputs: Minterm numbers, num variables, optional don't cares
 
 output: simplified expression
 
+helper class:
+- column/table class
 
 helper functions: 
 - calculate number of mismatching bits
+- count 1's in a number
 
+- vector to hold minterms
+- dictionary to hold index to vector of minterms
 */
+
+int8_t countOnes(int32_t n)
+{
+    int8_t res = 0;
+    for (int8_t i = 0; i < 32; i++)
+    {
+        if ((n >> i) & 1)
+            res++;
+    }
+    return res;
+}
 
 int8_t mismatchedBits(int32_t n1, int32_t n2)
 {
@@ -23,12 +39,9 @@ int8_t mismatchedBits(int32_t n1, int32_t n2)
     return res;
 }
 
-
 int main()
 {
-    
     std::cout << int(mismatchedBits(0, 1)) << std::endl;
-
     return 0;
 }
 
