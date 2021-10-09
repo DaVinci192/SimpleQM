8b084747952693452b3a7d973c72ddedd1db89e2
DaVinci192 <58537048+DaVinci192@users.noreply.github.com>
2021-10-09 13:11:36 -0400

    Added helper functions

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,11 @@
 #include <iostream>
+#include <map>
+#include <vector>
+#include <string>
+#include <sstream>
 #include "stdint.h"
+
+using namespace std;
 
 /* algorithm 
 Inputs: Minterm numbers, num variables, optional don't cares
@@ -9,13 +15,44 @@ output: simplified expression
 helper class:
 - column/table class
 
+- column class
+    entries: minterms/list of minterms, checked or not checked
+        store minterms as strings
+
+    entries separated (i.e. by number of 1's in first table)
+
 helper functions: 
 - calculate number of mismatching bits
 - count 1's in a number
-
-- vector to hold minterms
-- dictionary to hold index to vector of minterms
 */
+
+
+
+map<std::string, int32_t> toInt;
+void mapMinterms(map<std::string, int32_t> m, const vector<std::string> minterms, const vector<std::string> dontcares = {})
+{
+    for (int8_t i = 0; i < minterms.size(); i++)
+    {
+        m[minterms[i]] = stoi(minterms[i]);
+    }
+
+    for (int8_t i = 0; i < dontcares.size(); i++)
+    {
+        m[dontcares[i]] = stoi(dontcares[i]);
+    }
+}
+
+void sortMintermList(map<int8_t, vector<std::string>> mL, const vector<string> terms, const map<std::string, int32_t> termMap)
+{
+    for (int8_t i = 0; i < terms.size(); i++)
+    {
+        int8_t numOnes = countOnes(termMap[terms[i]]);
+        mL[numOnes].push_back(terms[i]);
+    }
+}
+
+
+vector<vector<int>> c1;
 
 int8_t countOnes(int32_t n)
 {
@@ -39,9 +76,32 @@ int8_t mismatchedBits(int32_t n1, int32_t n2)
     return res;
 }
 
+vector<std::string> getInput(std::string prompt)
+{
+    std::string line;
+    std::string term;
+    vector<std::string> terms;
+
+    std::cout << prompt;
+    std::getLine(std::cin, line);
+    std::istringstream stream(line);
+    while (stream >> term)
+    {
+        terms.push_back(term);
+    }
+    return terms;
+}
+
 int main()
 {
-    std::cout << int(mismatchedBits(0, 1)) << std::endl;
+    
+    vector<std::string> terms = getInput("Enter minterms here: ");
+
+    for (int i = 0; i < terms.size(); i++)
+    {
+        std::cout << terms[i] << std::endl;
+    }
+
     return 0;
 }
 
